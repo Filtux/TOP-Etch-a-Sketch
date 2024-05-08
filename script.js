5d5b867f6e609980341f0c703acd71a7aa380b75
Joe <joe@joematthews.com.au>
2024-05-08 17:51:15 +0930

    Completed javascript to a working state along with basic CSS

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
-gridContainer = document.querySelector(".grid-container");
-gridSize = 16
-squareSize = 100 / gridSize;
-
-for (let i = 0; i < gridSize * gridSize; i++) {
-    square = document.createElement("div")
-    square.className = 'grid-square';
-    square.style.width = squareSize + "%";
-    square.style.height = squareSize + "%";
-    gridContainer.appendChild(square);
-    square.addEventListener("mouseover", (e) => {
-        e.target.style.backgroundColor = "orange";
-    });
-}
-
+let gridContainer = document.querySelector(".grid-container");
 let slider = document.querySelector("#sliderRange");
 let output = document.querySelector("#sliderValue")
-output.innerHTML = slider.value;
+let resetButton = document.querySelector(".button-reset");  
+
+function createGrid(gridSize) {
+
+    resetGrid();
+
+    squareSize = 100 / gridSize;
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        square = document.createElement("div")
+        square.className = 'grid-square';
+        square.style.width = squareSize + "%";
+        square.style.height = squareSize + "%";
+        gridContainer.appendChild(square);
+        square.addEventListener("mouseover", (e) => {
+            e.target.style.backgroundColor = "orange";
+        });
+    }
+}
+
+function resetGrid() {
+    gridContainer.textContent = "";
+}
 
 slider.addEventListener("input", function() {
-    output.innerHTML = this.value;
+    output.textContent = slider.value;
+    createGrid(slider.value);
 });
+
+resetButton.addEventListener("click", function() {  
+    createGrid(slider.value);
+});
+
+createGrid(16);
+output.textContent = slider.value;
